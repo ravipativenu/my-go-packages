8ccc8c7167eefaa8f8194defda3944cce26e7d89
ravipativenu <venugopal.ravipati@gmail.com>
2021-10-19 13:53:52 -0500

    ..

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example/user/my-go-packages
+
+go 1.17
---
+++ my-go-packages.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"example/user/my-go-packages/morestrings"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+}
